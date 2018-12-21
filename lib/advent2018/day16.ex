5a5ebbbcb323a76b172ef36e1200fbf24e18fc77
Melinda Weathers <melindaweathers@gmail.com>
2018-12-21 17:39:07 -0500

    Day 16

--- lib/advent2018/day16.ex
+++ lib/advent2018/day16.ex
@@ -8,6 +8,12 @@ defmodule Day16 do
       |> Enum.to_list()
   end
 
+  def read_raw_instructions(filename) do
+    File.stream!(filename)
+      |> Stream.map(&(build_instruction_part(&1)))
+      |> Enum.to_list()
+  end
+
   #Before: [1, 1, 2, 0]
   #8 1 0 3
   #After:  [1, 1, 2, 1]
@@ -70,6 +76,7 @@ defmodule Day16 do
   end
 
   def matching_fns(sample), do: _matching_fns(sample, all_fns(), [])
+  def matching_fns(sample, fns), do: _matching_fns(sample, fns, [])
   defp _matching_fns(_, [], fns), do: fns
   defp _matching_fns(sample, [head|tail], fns) do
     {before, [_,a,b,c], aft} = sample
@@ -90,11 +97,35 @@ defmodule Day16 do
     end
   end
 
+  def execute_instructions(instructions, mappings) do
+    _execute_instructions(instructions, mappings, [0, 0, 0, 0])
+  end
+  defp _execute_instructions([], _, reg), do: reg
+  defp _execute_instructions([[i, a, b, c]|tail], mappings, reg) do
+    new_reg = apply(Day16, mappings[i], [[a,b,c],reg])
+    _execute_instructions(tail, mappings, new_reg)
+  end
+
+  def instruction_mappings(samples), do: _instruction_mappings(samples, %{})
+  defp _instruction_mappings([], mappings), do: mappings
+  defp _instruction_mappings([sample|tail], mappings) do
+    {before, [i,a,b,c], aft} = sample
+    possible_fns = Map.get(mappings, i, all_fns)
+    still_possible_fns = matching_fns(sample, possible_fns)
+    _instruction_mappings(tail, Map.put(mappings, i, still_possible_fns))
+  end
+
   def run do
     sample_samples = Day16.build_instructions("inputs/day16a-sample.txt")
     IO.inspect(find_over_three_matches(sample_samples), label: "First Sample")
 
     samples = Day16.build_instructions("inputs/day16a.txt")
     IO.inspect(find_over_three_matches(samples), label: "First Star")
+
+    IO.inspect(instruction_mappings(samples))
+    # Reduced by hand :O
+    mappings = %{0 => :eqir, 1 => :borr, 2 => :addr, 3 => :gtri, 4 => :muli, 5 => :gtir, 6 => :mulr, 7 => :banr, 8 => :bori, 9 => :eqri, 10 => :eqrr, 11 => :bani, 12 => :setr, 13 => :gtrr, 14 => :addi, 15 => :seti}
+    instructions = Day16.read_raw_instructions("inputs/day16b.txt")
+    IO.inspect(execute_instructions(instructions, mappings))
   end
 end
