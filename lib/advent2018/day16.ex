d70b2796957f14dead7a23c1b7993a1ddcfff738
Melinda Weathers <melindaweathers@gmail.com>
2018-12-20 19:13:20 -0500

    More day 16

--- lib/advent2018/day16.ex
+++ lib/advent2018/day16.ex
@@ -1,4 +1,5 @@
 defmodule Day16 do
+  use Bitwise
 
   def build_instructions(filename) do
     File.stream!(filename)
@@ -21,6 +22,30 @@ defmodule Day16 do
   defp _build_instruction_part(["Before:", "", a, b, c, d, ""]), do: [a,b,c,d]
   defp _build_instruction_part(["After:", "", "", a, b, c, d, ""]), do: [a,b,c,d]
   defp _build_instruction_part([a, b, c, d]), do: [a,b,c,d]
+
+  def regget(register, x), do: Enum.at(register, x)
+  def regset(register, x, val), do: List.update_at(register, x, fn(_) -> val end)
+
+  # addr (add register) stores into register C the result of adding register A and register B.
+  def addr([a, b, c], reg), do: regset(reg, c, regget(reg, a) + regget(reg, b))
+  # addi (add immediate) stores into register C the result of adding register A and value B.
+  def addi([a, b, c], reg), do: regset(reg, c, regget(reg, a) + b)
+
+  #mulr (multiply register) stores into register C the result of multiplying register A and register B.
+  def mulr([a, b, c], reg), do: regset(reg, c, regget(reg, a) * regget(reg, b))
+  #muli (multiply immediate) stores into register C the result of multiplying register A and value B.
+  def muli([a, b, c], reg), do: regset(reg, c, regget(reg, a) * b)
+
+  #banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+  def banr([a, b, c], reg), do: regset(reg, c, regget(reg, a) &&& regget(reg, b))
+  #bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+  def bani([a, b, c], reg), do: regset(reg, c, regget(reg, a) &&& b)
+
+  #borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
+  def borr([a, b, c], reg), do: regset(reg, c, bor(regget(reg, a), regget(reg, b)))
+  #bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
+  def bori([a, b, c], reg), do: regset(reg, c, bor(regget(reg, a), b))
+
 
 
   def run do
