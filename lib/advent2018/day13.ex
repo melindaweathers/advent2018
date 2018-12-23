2a6fa146845334423d92e994bb61a958b7ee280b
Melinda Weathers <melindaweathers@gmail.com>
2018-12-23 16:05:19 -0500

    Day 13 fixed

--- lib/advent2018/day13.ex
+++ lib/advent2018/day13.ex
@@ -24,39 +24,11 @@ defmodule Day13 do
   end
 
   def sort_carts(carts) do
-    Enum.sort(carts, fn(cart1,cart2) -> cart1.y <= cart2.y && cart1.x <= cart2.x end)
+    Enum.sort(carts, fn(cart1,cart2) -> (cart1.y < cart2.y) || (cart1.y == cart2.y && cart1.x <= cart2.x) end)
   end
 
-  def collision?(moving_cart, all_carts) do
-    Enum.count(all_carts, fn cart -> cart.x == moving_cart.x && cart.y == moving_cart.y end) >= 1
-  end
-
-  def move_carts(track, carts), do: _move_carts(track, carts, [])
-  defp _move_carts(_, [], moved_carts), do: {:ok, sort_carts(moved_carts)}
-  defp _move_carts(track, [cart|tail], moved_carts) do
-    new_cart = move_cart(track, cart)
-    if collision?(new_cart, tail ++ moved_carts) do
-      {:collision, [new_cart]}
-    else
-      _move_carts(track, tail, [new_cart|moved_carts])
-    end
-  end
-
-  def move_cart(track, cart) do
-    {x, y} = next_cart_pos(cart)
-    {dir, turns} = next_cart_orientation(cart, track[y][x])
-    %Cart{direction: dir, x: x, y: y, turns: turns}
-  end
-
-  def run_carts(track, carts) do
-    {status, new_carts} = move_carts(track, carts)
-    if status == :collision do
-      [cart|[]] = new_carts
-      {cart.x, cart.y}
-    else
-      #IO.inspect(new_carts)
-      run_carts(track, new_carts)
-    end
+  def collision?(moving_cart, other_carts) do
+    Enum.any?(other_carts, fn cart -> cart.x == moving_cart.x && cart.y == moving_cart.y end)
   end
 
   def next_cart_pos(%Cart{direction: ">"} = cart), do: {cart.x + 1, cart.y}
@@ -80,8 +52,10 @@ defmodule Day13 do
   end
   def next_cart_orientation(cart, char), do: {next_direction(cart.direction, char), cart.turns}
 
-  def next_direction(dir, "-"), do: dir
-  def next_direction(dir, "|"), do: dir
+  def next_direction(">", "-"), do: ">"
+  def next_direction("<", "-"), do: "<"
+  def next_direction("^", "|"), do: "^"
+  def next_direction("v", "|"), do: "v"
   def next_direction("^", "/"), do: ">"
   def next_direction("v", "/"), do: "<"
   def next_direction("<", "/"), do: "v"
@@ -91,12 +65,54 @@ defmodule Day13 do
   def next_direction("<", "\\"), do: "^"
   def next_direction(">", "\\"), do: "v"
 
+  def move_cart(track, cart) do
+    {x, y} = next_cart_pos(cart)
+    {dir, turns} = next_cart_orientation(cart, track[y][x])
+    %Cart{direction: dir, x: x, y: y, turns: turns}
+  end
+
+  def move_carts(track, carts), do: _move_carts(track, carts, [])
+  defp _move_carts(track, [], moved_carts), do: _move_carts(track, sort_carts(moved_carts), [])
+  defp _move_carts(track, [cart|tail], moved_carts) do
+    new_cart = move_cart(track, cart)
+    #IO.inspect(new_cart)
+    if collision?(new_cart, tail ++ moved_carts) do
+      {new_cart.x, new_cart.y}
+    else
+      _move_carts(track, tail, [new_cart|moved_carts])
+    end
+  end
+
+  def find_last_cart(track, carts), do: _find_last_cart(track, sort_carts(carts), [])
+  defp _find_last_cart(_, [], [cart]), do: {cart.x, cart.y}
+  defp _find_last_cart(track, [], moved_carts), do: _find_last_cart(track, sort_carts(moved_carts), [])
+  defp _find_last_cart(track, [cart|tail], moved_carts) do
+    new_cart = move_cart(track, cart)
+    #IO.inspect(new_cart)
+    cond do
+      collision?(new_cart, tail) ->
+        IO.inspect("Collision at #{new_cart.x},#{new_cart.y}")
+        new_tail = Enum.reject(tail, fn cart -> cart.x == new_cart.x && cart.y == new_cart.y end)
+        _find_last_cart(track, new_tail, moved_carts)
+      collision?(new_cart, moved_carts) ->
+        IO.inspect("Collision at #{new_cart.x},#{new_cart.y}")
+        new_moved_carts = Enum.reject(moved_carts, fn cart -> cart.x == new_cart.x && cart.y == new_cart.y end)
+        _find_last_cart(track, tail, new_moved_carts)
+      true ->
+        _find_last_cart(track, tail, [new_cart|moved_carts])
+    end
+  end
+
   def run do
     {sample_track, sample_carts} = (load_track("inputs/day13-sample.txt"))
-    IO.inspect(run_carts(sample_track, sample_carts), label: "Should be 7,3")
+    IO.inspect(move_carts(sample_track, sample_carts), label: "Should be 7,3")
 
-    #NOPE (You guessed 119,9.)
+    {sample_trackb, sample_cartsb} = (load_track("inputs/day13b-sample.txt"))
+    IO.inspect(find_last_cart(sample_trackb, sample_cartsb), label: "Should be 6,4")
+
     {track, carts} = (load_track("inputs/day13.txt"))
-    IO.inspect(run_carts(track, carts), label: "First Star")
+    IO.inspect(move_carts(track, carts), label: "First Star")
+
+    IO.inspect(find_last_cart(track, carts), label: "Second star")
   end
 end
